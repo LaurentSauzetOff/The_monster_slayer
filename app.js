6d267cf5a92640134b9636d92fa9e4c0da8d39e4
Laurent Sauzet <59488676+LaurentSauzetOff@users.noreply.github.com>
2021-12-19 15:06:21 +0100

    Finishing the log

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function () {
         let damage = this.calculateDamage(3, 10);
@@ -28,8 +29,12 @@ new Vue({
 
     },
     specialAttack: function () {
-        this.monsterHealth -= this.calculateDamage(10, 20);
-
+        let damage = this.calculateDamage(10, 20);
+        this.monsterHealth -= damage;
+        this.turns.unshift({
+            isPlayer : true,
+            text: 'Vous avez infligé au monstre ' + damage + ' points de gros dégats.'
+        })
       if (this.checkWin()) {
         return;
       }
@@ -41,6 +46,10 @@ new Vue({
         } else {
             this.playerHealth = 100;
         }
+        this.turns.unshift({
+            isPlayer : true,
+            text: 'Vous avez remonté vore santé de '
+        })
         this.monsterAttacks();
     },
     giveUp: function () {
